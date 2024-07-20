438b8e4655f0012af3b6d93fb7a1911305ca1f18
JustDingel <Daniel.Scharfenberg@hotmail.de>
2024-07-20 13:05:14 +0200

    Update install_panel.sh

--- installers/install_panel.sh
+++ installers/install_panel.sh
@@ -387,21 +387,10 @@ echo -e "${GREEN}Installation completed successfully!${NC}"
 echo -e "${YELLOW}Installing Webserver...${NC}"
 
 if [[ $APPLICATION_URL == http://* ]]; then
+    domain_without_protocol=$(echo $APPLICATION_URL | sed -e 's#^http://##' -e 's#^https://##')
     echo -e "${YELLOW}HTTP detected. Proceeding without SSL...${NC}"
-    # Hier die Schritte für HTTP ohne SSL einfügen
-elif [[ $APPLICATION_URL == https://* ]]; then
-    echo -e "${YELLOW}HTTPS detected. Proceeding with SSL...${NC}"
-    # Hier die Schritte für HTTPS mit SSL einfügen
-else
-    echo -e "${RED}Invalid input. Please make sure to include http:// or https:// in the domain.${NC}"
-    exit 1
-fi
-
-domain_without_protocol=$(echo $APPLICATION_URL | sed -e 's#^http://##' -e 's#^https://##')
-
-sudo rm /etc/nginx/sites-enabled/default
-
-read -r -d '' PTEROQ_NGINX_SERVICE << EOM
+    sudo rm /etc/nginx/sites-enabled/default
+    read -r -d '' PTEROQ_NGINX_SERVICE << EOM
 
 server {
     # Replace the example <domain> with your domain name or IP address
@@ -450,10 +439,96 @@ server {
     }
 }
 EOM
-echo -e "${YELLOW}Creating pteroqdactyl.conf file...${NC}"
-echo "$PTEROQ_NGINX_SERVICE" | sudo tee /etc/nginx/sites-available/pterodactyl.conf > /dev/null
-echo -e "${YELLOW}Symlink file...${NC}"
-sudo ln -s /etc/nginx/sites-available/pterodactyl.conf /etc/nginx/sites-enabled/pterodactyl.conf
-echo -e "${YELLOW}Restarting Nginx...${NC}"
-sudo systemctl restart nginx
-echo -e "${GREEN}Panel installation successfully${NC}"
+    echo -e "${YELLOW}Creating pteroqdactyl.conf file...${NC}"
+    echo "$PTEROQ_NGINX_SERVICE" | sudo tee /etc/nginx/sites-available/pterodactyl.conf > /dev/null
+    echo -e "${YELLOW}Symlink file...${NC}"
+    sudo ln -s /etc/nginx/sites-available/pterodactyl.conf /etc/nginx/sites-enabled/pterodactyl.conf
+    echo -e "${YELLOW}Restarting Nginx...${NC}"
+    sudo systemctl restart nginx
+    echo -e "${GREEN}Panel installation successfully${NC}"
+    
+    # Hier die Schritte für HTTP ohne SSL einfügen
+elif [[ $APPLICATION_URL == https://* ]]; then
+    domain_without_protocol=$(echo $APPLICATION_URL | sed -e 's#^http://##' -e 's#^https://##')
+    echo -e "${YELLOW}HTTPS detected. Proceeding with SSL...${NC}"
+    sudo rm /etc/nginx/sites-enabled/default
+    read -r -d '' PTEROQ_NGINX_SERVICE << EOM
+server_tokens off;
+
+server {
+    listen 80;
+    server_name $domain_without_protocol;
+    return 301 https://\$server_name$request_uri;
+}
+
+server {
+    listen 443 ssl http2;
+    server_name $domain_without_protocol;
+
+    root /var/www/pterodactyl/public;
+    index index.php;
+
+    access_log /var/log/nginx/pterodactyl.app-access.log;
+    error_log  /var/log/nginx/pterodactyl.app-error.log error;
+
+    # allow larger file uploads and longer script runtimes
+    client_max_body_size 100m;
+    client_body_timeout 120s;
+
+    sendfile off;
+
+    # SSL Configuration - Replace the example <domain> with your domain
+    ssl_certificate /etc/letsencrypt/live/$domain_without_protocol/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/$domain_without_protocol/privkey.pem;
+    ssl_session_cache shared:SSL:10m;
+    ssl_protocols TLSv1.2 TLSv1.3;
+    ssl_ciphers "ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384";
+    ssl_prefer_server_ciphers on;
+
+    # See https://hstspreload.org/ before uncommenting the line below.
+    # add_header Strict-Transport-Security "max-age=15768000; preload;";
+    add_header X-Content-Type-Options nosniff;
+    add_header X-XSS-Protection "1; mode=block";
+    add_header X-Robots-Tag none;
+    add_header Content-Security-Policy "frame-ancestors 'self'";
+    add_header X-Frame-Options DENY;
+    add_header Referrer-Policy same-origin;
+
+    location / {
+        try_files \$uri \$uri/ /index.php?\$query_string;
+    }
+
+    location ~ \.php$ {
+        fastcgi_split_path_info ^(.+\.php)(/.+)$;
+        fastcgi_pass unix:/run/php/php8.1-fpm.sock;
+        fastcgi_index index.php;
+        include fastcgi_params;
+        fastcgi_param PHP_VALUE "upload_max_filesize = 100M \n post_max_size=100M";
+        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+        fastcgi_param HTTP_PROXY "";
+        fastcgi_intercept_errors off;
+        fastcgi_buffer_size 16k;
+        fastcgi_buffers 4 16k;
+        fastcgi_connect_timeout 300;
+        fastcgi_send_timeout 300;
+        fastcgi_read_timeout 300;
+        include /etc/nginx/fastcgi_params;
+    }
+
+    location ~ /\.ht {
+        deny all;
+    }
+}
+EOM
+    echo -e "${YELLOW}Creating pteroqdactyl.conf file...${NC}"
+    echo "$PTEROQ_NGINX_SERVICE" | sudo tee /etc/nginx/sites-available/pterodactyl.conf > /dev/null
+    echo -e "${YELLOW}Symlink file...${NC}"
+    sudo ln -s /etc/nginx/sites-available/pterodactyl.conf /etc/nginx/sites-enabled/pterodactyl.conf
+    echo -e "${YELLOW}Restarting Nginx...${NC}"
+    sudo systemctl restart nginx
+    echo -e "${GREEN}Panel installation successfully${NC}"
+    # Hier die Schritte für HTTPS mit SSL einfügen
+else
+    echo -e "${RED}Invalid input. Please make sure to include http:// or https:// in the domain.${NC}"
+    exit 1
+fi
